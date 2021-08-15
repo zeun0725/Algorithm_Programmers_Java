60299a95630b6443618f48a5674c811cedb8ca7f
parkjieun <wldmsdl3187@naver.com>
2021-08-15 23:54:07 +0900

    class

---
+++ src/com/fastcampus/classpart/Student.java
@@ -0,0 +1,12 @@
+package com.fastcampus.classpart;
+
+public class Student {
+    public int studentID;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentName + "," + address);
+    }
+
+}
---
+++ src/com/fastcampus/classpart/StudentTest.java
@@ -0,0 +1,11 @@
+package com.fastcampus.classpart;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "테스트";
+        studentLee.address = "seoul";
+
+        studentLee.showStudentInfo();
+    }
+}
