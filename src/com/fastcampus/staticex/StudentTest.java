d6ee077b118c111765b29ebd2628b24e32a16b87
parkjieun <wldmsdl3187@naver.com>
2021-08-30 00:30:49 +0900

    static

--- src/com/fastcampus/staticex/StudentTest.java
+++ src/com/fastcampus/staticex/StudentTest.java
@@ -3,6 +3,7 @@ package com.fastcampus.staticex;
 public class StudentTest {
     public static void main(String[] args) {
         Student Lee = new Student("lee");
+        //static 메서드는 인스턴스 변수와 상관 없이 클래스 이름으로 접근 가능
         System.out.println(Student.getSerialNum());
         System.out.println(Lee.getStudentID());
         Student Kim = new Student("kim");
