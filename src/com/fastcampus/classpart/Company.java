65539c8447d983ccb0876d5ffea757fb57b50035
parkjieun <wldmsdl3187@naver.com>
2021-09-02 23:45:36 +0900

    singleton pattern

--- src/com/fastcampus/classpart/Company.java
+++ src/com/fastcampus/classpart/Company.java
@@ -3,6 +3,8 @@ package com.fastcampus.classpart;
 public class Company {
     private static Company instance = new Company(); //유일한 애
 
+    private Company() {}
+
     public static Company getInstance() {
         if( instance == null) {
             instance = new Company();
