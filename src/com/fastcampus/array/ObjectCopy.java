d460b5b906ef44a4c4c5b772b0a592fea5eb329a
parkjieun <wldmsdl3187@naver.com>
2021-09-04 00:50:26 +0900

    array

--- src/com/fastcampus/array/ObjectCopy.java
+++ src/com/fastcampus/array/ObjectCopy.java
@@ -10,11 +10,43 @@ public class ObjectCopy {
         library[3] = new Book("태백산맥", "조정래");
         library[4] = new Book("태백산맥", "조정래");
 
+        //얕은 복사(같은 인스턴스)
         System.arraycopy(library, 0, copyLibrary, 0, 5);
         for(Book book: copyLibrary) {
             //향상된 for문
             System.out.println(book);
             book.showBookInfo();
         }
+
+        library[0].setTitle("나목");
+        library[0].setAuthor("박완서");
+
+        for(Book book: copyLibrary) {
+            //향상된 for문
+            System.out.println(book);
+            book.showBookInfo();
+        }
+
+        copyLibrary[0] = new Book();
+        copyLibrary[1] = new Book();
+        copyLibrary[2] = new Book();
+        copyLibrary[3] = new Book();
+        copyLibrary[4] = new Book();
+
+        //깊은 복사(다른 인스턴스)
+        for(int i=0; i<copyLibrary.length; i++) {
+            copyLibrary[i].setAuthor(library[i].getAuthor());
+            copyLibrary[i].setTitle(library[i].getTitle());
+        }
+
+        library[0].setTitle("나목에서 바뀜");
+        library[0].setAuthor("박완서2");
+
+        for(Book book: copyLibrary) {
+            //향상된 for문
+            System.out.println(book);
+            book.showBookInfo();
+        }
+
     }
 }
