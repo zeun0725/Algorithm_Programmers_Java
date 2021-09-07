68fad01436f42924a316b287e0d14ab44939570a
parkjieun <wldmsdl3187@naver.com>
2021-09-07 22:18:32 +0900

    arraylist

--- src/com/fastcampus/array/ArrayListTest.java
+++ src/com/fastcampus/array/ArrayListTest.java
@@ -4,18 +4,15 @@ import java.util.ArrayList;
 
 public class ArrayListTest {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList list = new ArrayList();
 
         list.add("aaa");
         list.add("bbb");
         list.add("ccc");
 
-        for(String _list : list) {
+        for(Object _list : list) {
             System.out.println(_list);
         }
 
-        for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
-        }
     }
 }
