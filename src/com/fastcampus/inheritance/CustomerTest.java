2128b04bcc0655d2af5f9a51edbcbeeb6bd611e0
parkjieun <wldmsdl3187@naver.com>
2021-09-08 23:38:42 +0900

    inheritance

--- src/com/fastcampus/inheritance/CustomerTest.java
+++ src/com/fastcampus/inheritance/CustomerTest.java
@@ -2,16 +2,19 @@ package com.fastcampus.inheritance;
 
 public class CustomerTest {
     public static void main(String[] args) {
-//        Customer customerLee = new Customer();
-//        customerLee.setCustomerName("이순신");
-//        customerLee.setCustomerID(10010);
-//        customerLee.bonusPoint = 1000;
-//        System.out.println(customerLee.showCustomerInfo());
+        Customer customerLee = new Customer(10010,"이순신");
+        customerLee.setCustomerName("이순신");
+        customerLee.setCustomerID(10010);
+        customerLee.bonusPoint = 1000;
+        System.out.println(customerLee.showCustomerInfo());
 
-        VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
+        Customer customerKim = new VIPCustomer(10020, "김유신");
         customerKim.setCustomerName("김유신");
         customerKim.setCustomerID(10020);
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.showCustomerInfo());
+
+        customerLee = customerKim; //업캐스팅
+
     }
 }
