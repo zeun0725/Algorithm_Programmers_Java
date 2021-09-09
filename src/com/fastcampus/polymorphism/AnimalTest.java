1bfc02dd87fd6806f9b3ce66746474f381ecce84
parkjieun <wldmsdl3187@naver.com>
2021-09-10 00:30:21 +0900

    downcasting

--- src/com/fastcampus/polymorphism/AnimalTest.java
+++ src/com/fastcampus/polymorphism/AnimalTest.java
@@ -12,11 +12,19 @@ class Human extends Animal {
     public void move() {
         System.out.println("사람이 움직입니다.");
     }
+
+    public void readBooks() {
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
 
 class Tiger extends Animal {
     public void move() {
         System.out.println("호랑이가 네발로 뜁니다.");
+    }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
     }
 }
 
@@ -26,7 +34,8 @@ public class AnimalTest {
         // 다형성 (polymorphism)
         Animal hAnimal = new Human();
         Animal tAnimal = new Tiger();
-//        AnimalTest test = new AnimalTest();
+
+        AnimalTest test = new AnimalTest();
 //        test.moveAnimal(hAnimal);
 //        test.moveAnimal(tAnimal);
 
@@ -36,6 +45,24 @@ public class AnimalTest {
 
         for(Animal animal : animalList) {
             animal.move();
+
+        }
+
+        test.testDownCasting(animalList);
+    }
+
+    public void testDownCasting(ArrayList<Animal> list) {
+        for(int i=0; i<list.size(); i++) {
+            Animal ani = list.get(i);
+
+            if ( ani instanceof Human ) {
+                Human human = (Human)ani;
+                human.readBooks();
+            }
+            else if (ani instanceof Tiger) {
+                Tiger tiger = (Tiger)ani;
+                tiger.hunting();
+            }
         }
     }
 
